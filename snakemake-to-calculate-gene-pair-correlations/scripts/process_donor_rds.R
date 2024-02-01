c91679fc62cd4bb2b9b6465a37f7ae061e44e35f
Anoek Kooijmans <a.kooijmans@st.hanze.nl>
2024-02-01 13:57:59 +0100

    All changes made to process_donor_rds

--- snakemake-to-calculate-gene-pair-correlations/scripts/process_donor_rds.R
+++ snakemake-to-calculate-gene-pair-correlations/scripts/process_donor_rds.R
@@ -1,134 +1,155 @@
+.libPaths("/usr/local/lib/R/site-library")
+
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("Seurat"))
+suppressPackageStartupMessages(library("yaml"))
+suppressPackageStartupMessages(library("tidyr"))
+suppressPackageStartupMessages(library("weights"))
 
-#calculating correlations for single donor, to allow for parallelisation in snakemake pipeline
+option_list <- list(
+  make_option("--celltype", type = "character", help = "Cell type"),
+  make_option("--cohort", type = "character", help = "Cohort ID"),
+  make_option("--n", type = "integer", help = "Number of genes"),
+  make_option("--donor", type = "character", help = "Donor"),
+  make_option("--method", type = "character", help = "Correlation method"),
+  make_option("--weight", type = "character", help = "Weight for correlation method. Either 'weighted' or 'unweighted'"),
+  make_option("--genelist", type = "character", help = "List of genes"),
+  make_option("--input", type = "character", help = "Input donor rds file"),
+  make_option("--output", type = "character", help = "Output directory"),
+  make_option("--metadata", type = "logical", help = "Include metadata", default = FALSE)
+)
 
-library(Seurat)
-library(matrixStats)
-library(weights)
-library(stringr)
+parser <- OptionParser(option_list = option_list)
+args <- parse_args(parser)
 
-# functions
-weighted_corr <- function(normalized_counts_mtr_d, raw_counts_mtr_d,method){
-	  
-  if(method =='spearman'){
-    for(i in 1:nrow(normalized_counts_mtr_d)){
-      normalized_counts_mtr_d[i,] <- rank(normalized_counts_mtr_d[i,])
-    }
-  } else {
-      normalized_counts_mtr_d = normalized_counts_mtr_d
-  }
+print("Options in effect:")
+for (name in names(args)) {
+  print(paste0("  --", name, " ", args[[name]]))
+}
+
+# Define functions
+calculate_correlation <- function(n_counts, raw_counts, method, weighted) {
   
-  corMat = wtd.cor(t(normalized_counts_mtr_d),weight = (colSums(raw_counts_mtr_d!=0)))
-  corVec <- corMat$correlation[upper.tri(corMat$correlation)]
-  pvVec <- corMat$p.value[upper.tri(corMat$p.value)]
-  pvVec[pvVec<1.0e-150] = 1.0e-150
-  ZscMat <- abs(qnorm(pvVec/2))
-  ZscMat[corVec<0] <- ZscMat[corVec<0]*-1
-  mat_pval <- as.data.frame(cbind(corVec, pvVec,ZscMat))
+  # Check if input is correct
+  if (!(method %in% c('pearson', 'spearman'))) {
+    stop("Invalid method. Use 'pearson' or 'spearman'.")}
+
+  if (!is.logical(weighted)) {
+    stop("Invalid 'weighted' argument. Use TRUE or FALSE.")}
+
+  # Calculate correlation
+  if (method == 'spearman'){
+    for (i in 1:nrow(n_counts)) { n_counts[i,] <- rank(n_counts[i,]) }}
+  
+  if (weighted) { weights <- as.data.frame(colSums(raw_counts != 0))[, 1]
+  } else { weights <- rep(1, ncol(raw_counts)) }
+  
+  corMat <- wtd.cor(t(n_counts), weight=weights)
+    
+  return(corMat)
+}
+
+get_cor_stats <- function(corMat) {
+  
+  # Get correlation, p-value and z-score
+  corr <- corMat$correlation[upper.tri(corMat$correlation)]
+  
+  pval <- corMat$p.value[upper.tri(corMat$p.value)]
+  pval[pval < 1.0e-150] <- 1.0e-150
+  
+  zscore <- abs(qnorm(pval/2))
+  zscore[corr < 0] <- zscore[corr < 0] * -1
+  
+  cor_df <- as.data.frame(cbind(corr, pval, zscore))
+  
+  # Get gene-pairs
   expanded <- expand.grid(rownames(corMat$correlation), rownames(corMat$correlation))
-  mat <- matrix(paste(expanded$Var1, expanded$Var2, sep="_"), nrow=length(rownames(corMat$correlation)), ncol=length(rownames(corMat$correlation)))
-  gene_p <- mat[upper.tri(mat)]
-	    
-  rownames(mat_pval) <- gene_p
-  return(mat_pval)
-}
-
-
-get_corr_values_donor_from_rds_file <- function(donor,dir_with_seurat,cohort_id,output_dir,gene_pairs,cell_type,gene_names){
-
-  cell_type_data <- readRDS(paste0(dir_with_seurat,cell_type,'-',donor,'.rds'))
-  cell_type_data_S <- cell_type_data[rownames(gene_names),]
-        
-  # Getting a number of counts per cell
-  raw_counts_mtr <- as.data.frame(as.matrix(cell_type_data_S@assays$RNA@counts ))
-        
-  number_of_counts <- as.data.frame(colSums(raw_counts_mtr))
-  ncells_per_donor <- as.data.frame(table(cell_type_data$Assignment))
-	  
-  raw_counts_mtr_d <- as.data.frame(as.matrix(cell_type_data_S@assays$RNA@counts ))
-  normalized_counts_mtr_d <- as.data.frame(as.matrix(cell_type_data_S@assays$data@data ))
-	    
-  rm(cell_type_data_S)
-  rm(cell_type_data)
-	      
-  raw_counts_mtr_d_non_zero <- as.data.frame(rowSums(raw_counts_mtr_d != 0))
-  colnames(raw_counts_mtr_d_non_zero) <- 'counts'
-	        
-  raw_counts_mtr_d_non_zero$mean <- rowMeans(raw_counts_mtr_d)  # save
-  raw_counts_mtr_d_non_zero$UMI <- rowSums(raw_counts_mtr_d)  # save
-  genes_with_more_than_10_non_zero <- rownames(raw_counts_mtr_d_non_zero[raw_counts_mtr_d_non_zero$counts >10,]) # save
-  raw_counts_mtr_d_non_zero <- raw_counts_mtr_d_non_zero[rownames(raw_counts_mtr_d_non_zero) %in%genes_with_more_than_10_non_zero, ]
-  raw_counts_mtr_d_UMI <- as.data.frame(colSums(raw_counts_mtr_d != 0))
-  colnames(raw_counts_mtr_d_UMI) <- 'UMI'
-
-  normalized_counts_mtr_d <- normalized_counts_mtr_d[rownames(normalized_counts_mtr_d) %in%genes_with_more_than_10_non_zero, ]
-
-  #getting PCC and SCC  
   
-  print("output directory:")
-  method = 'pearson'
-  print(gsub(' ','',paste0(output_dir,cohort_id,'/','zscore_',cell_type,"_",donor,"_",method,'_weighted.tsv.gz')))
-  print("calculating Pearson weighted correlation")
-  mat_pval_PCC <- weighted_corr(normalized_counts_mtr_d, raw_counts_mtr_d, method)
-  mat_pval_PCC$x <- rownames(mat_pval_PCC)
-  mat_pval_PCC <- merge(gene_pairs, mat_pval_PCC, by='x' , all.x=T)
-  rownames(mat_pval_PCC) <- mat_pval_PCC$x
-
-  corr_w_gz = gzfile(gsub(' ','',paste0(output_dir,cohort_id,'/','corr-',cell_type,"-",donor,"-",method,'-weighted.tsv.gz')),'w')
-  write.table(paste(rownames(mat_pval_PCC),mat_pval_PCC$corVec,sep='\t'), corr_w_gz,sep='\t', row.names = F, quote = F)
-  close(corr_w_gz)
-
-  pval_w_gz = gzfile(gsub(' ','',paste0(output_dir,cohort_id,'/','pval-',cell_type,"-",donor,"-",method,'-weighted.tsv.gz')),'w')
-  write.table(paste(rownames(mat_pval_PCC),mat_pval_PCC$pvVec,sep='\t'), pval_w_gz,sep='\t', row.names = F, quote = F)
-  close(pval_w_gz)
-
-  zsco_w_gz = gzfile(gsub(' ','',paste0(output_dir,cohort_id,'/','zscore-',cell_type,"-",donor,"-",method,'-weighted.tsv.gz')),'w')
-  write.table(paste(rownames(mat_pval_PCC),mat_pval_PCC$ZscMat,sep='\t'), zsco_w_gz,sep='\t', row.names = F, quote = F)
-  close(zsco_w_gz)
-
-  print("done.")  
+  mat <- matrix(paste(expanded$Var1, expanded$Var2, sep = "_"), 
+                nrow = length(rownames(corMat$correlation)), 
+                ncol = length(rownames(corMat$correlation)))
+  
+  gene_p <- mat[upper.tri(mat)]
+  
+  rownames(cor_df) <- gene_p
+  
+  return(cor_df)
 }
 
-
-cell_type = args[1]
-cohort_id = args[2]
-donor = args[3]
-gene_list_file = args[4]
-path_for_donor_rds = args[5]
-output_dir = args[6]
-alt_gene_list = args[7]
-
-# selection of genes 
-
-print(paste("Cell type:",cell_type))
-print(paste("Cohort:",cohort_id))
-print(paste("Donor:",donor))
-print(paste("gene list file:",gene_list_file))
-print(paste("donor rds path:",path_for_donor_rds))
-print(paste("output directory:",output_dir))
-print(paste("alternative gene list path:",alt_gene_list))
-
-print("Calculating all possible gene pairs unidirectionally")
-if(alt_gene_list == 'nan'){
-  gene_names = as.data.frame(read.table(gene_list_file,header=T)[,1])
-  colnames(gene_names) = c('gene')
-  rownames(gene_names) = gene_names$gene
-} else {
-  gene_names = as.data.frame(read.table(alt_gene_list, header=T)[,1])
-  colnames(gene_names) = c('gene')
-  rownames(gene_names) = gene_names$gene
-}
-print(paste("Number of genes in list:",length(gene_names$gene)))
-
-gene_pairs = c()
-n = 1
-for (i in seq(length(gene_names$gene)-1)){ 
-  gene_pairs = c(gene_pairs, paste(rep(gene_names$gene[n],(length(gene_names$gene)-n)),gene_names$gene[(n+1):length(gene_names$gene)],sep='_')) 
-  n = n+1
+get_paired_zeros <- function(counts, gene_pairs) {
+  
+  paired_zeros <- c()
+  total_zeros <- c()
+  
+  for (x in gene_pairs) {
+    g <- strsplit(x, "_")[[1]]
+    gene1 <- counts[g[1],]
+    gene2 <- counts[g[2],]
+    
+    count1 <- which(gene1 %in% 0)
+    count2 <- which(gene2 %in% 0)
+    
+    paired_zeros <- append(paired_zeros, length(intersect(count1,count2)))
+    total_zeros <- append(total_zeros, length(count1) + length(count2))
+  }
+  df <- cbind.data.frame(paired_zeros,total_zeros)
+  rownames(df) <- gene_pairs
+  return(df)
 }
 
-print("Starting correlation functions.")
-get_corr_values_donor_from_rds_file(donor,path_for_donor_rds,cohort_id,output_dir,gene_pairs,cell_type,gene_names)
+cat("\n\nLoading list of genes")
+gene_list <- read.table(gzfile(args$genelist), header = FALSE, sep = "\t")$V1
+cat("\nNumber of genes loaded:", length(gene_list))
 
+cat("\n\nLoading RDS file:",args$input)
+sc_data <- readRDS(args$input)
 
+cat("\nExtracting counts")  
+n_counts <- as.data.frame(as.matrix(sc_data@assays$data@data))
+raw_counts <- as.data.frame(as.matrix(sc_data@assays$RNA@counts))
+rm(sc_data)
+
+# Fitering on counts > 10 non-zero values
+raw_counts_df <- as.data.frame(rowSums(raw_counts != 0))
+raw_counts_df$gene_name <- rownames(raw_counts_df)
+colnames(raw_counts_df) <- c('counts','gene_names')
+filtered_genes <- raw_counts_df$gene_names[raw_counts_df$counts > 10] ## TODO: add variable for number to be filtered?
+
+raw_counts_filtered <- raw_counts[rownames(raw_counts) %in% filtered_genes, ]
+n_counts_filtered <- n_counts[rownames(n_counts) %in% filtered_genes, ]
+
+gene_pairs <- combn(gene_list, 2, FUN = function(x) paste(x, collapse = "_"), simplify = TRUE)
+cat("\nNumber of unique gene pairs:", length(gene_pairs))
+
+if (args$metadata) {
+  cat("Saving metadata")
+  paired_zero_counts <- get_paired_zeros(raw_counts,gene_pairs)
+  gene_count_df <- as.data.frame(list(counts_sum = rowSums(raw_counts),
+                                      counts_mean = rowMeans(raw_counts),
+                                      counts_sd = apply(raw_counts, 1, sd),
+                                      zero_counts = rowSums(raw_counts == 0)))
+  write.table(paired_zero_counts, file = gzfile(paste0(args$output,args$cohort,"/",args$donor,"-gene-pair-metadata.tsv.gz")), sep = "\t", quote = FALSE, row.names = TRUE) 
+  write.table(gene_count_df, file = gzfile(paste0(args$output,args$cohort,"/",args$donor,"-gene-count-metadata.tsv.gz")), sep = "\t", quote = FALSE, row.names = TRUE)
+}
+
+cat("\n\nCalculating correlation:",args$weight,args$method)
+weighted <- ifelse(args$weight == 'weighted', TRUE, FALSE)
+corMat <- calculate_correlation(n_counts_filtered, raw_counts_filtered, args$method, weighted)
+
+rm(n_counts,n_counts_filtered,raw_counts,raw_counts_filtered)
+
+cat("\nExtracting correlation for unique gene pairs\n")
+cor_df <- get_cor_stats(corMat)
+cor_df$x <- rownames(cor_df)
+cor_df <- merge(gene_pairs, cor_df, by='x' , all.x=T)
+rownames(cor_df) <- cor_df$x
+rm(corMat)
+
+metrics = c("corr","pval","zscore")
+cat("\n\nSaving correlation for each metric\n\n")
+for (x in metrics){
+    out_path <- paste0(args$output,args$cohort,"/",args$donor,"-",x,"-",args$celltype,"-top-",args$n,"-",args$method,"-",args$weight,".tsv.gz")
+    write.table(cor_df[x], gzfile(out_path),sep="\t",row.names = TRUE, quote = FALSE)
+    cat(x, "\nSaved to", out_path,"\n")
+  }
