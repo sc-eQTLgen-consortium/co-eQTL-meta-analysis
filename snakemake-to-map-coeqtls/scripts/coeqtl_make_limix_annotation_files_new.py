75ce4363ecbbc93f7db0b6677f1cd13a1b380f33
Roy Oelen <roy.oelen@gmail.com>
2024-10-03 11:47:46 +0200

    - example in header

--- snakemake-to-map-coeqtls/scripts/coeqtl_make_limix_annotation_files_new.py
+++ snakemake-to-map-coeqtls/scripts/coeqtl_make_limix_annotation_files_new.py
@@ -1,7 +1,17 @@
 """
-This script is creating gene annotation files
+This script is creating gene annotation files for the co-eQTL mapping pipeline
 
 authors: Dan Kaptijn, Marc-Jan Bonder, Roy Oelen
+
+example usage:
+
+python coeqtl_make_limix_annotation_files_new.py \
+    --gwas_loc /groups/umcg-franke-scrna/tmp04/projects/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_wijst2018/sceqtlgen_coeqtl_map/GWAS_snp_gene_pairs_immune_related_disease.txt.gz \
+    --gene_loc /groups/umcg-franke-scrna/tmp04/projects/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_wijst2018/calculate_correlation_metrics/gene_lists/F11_Decision_Tree_Geneswg3_wijst2018_Mono.Qced.Normalized.SCs.Rds.tsv.gz \
+    --qtl_loc /groups/umcg-franke-scrna/tmp04/projects/sc-eqtlgen-consortium-pipeline/ongoing/wg3/wg3_wijst2018/sceqtlgen_coeqtl_map/eQTLs_finemapped_20240626/Mono.DS.wg3_Ye_wg3_wijst2018_wg3_sawcer_wg3_oneK1K_wg3_okada_wg3_Li_wg3_Franke_split_v3_wg3_Franke_split_v2_wg3_multiome_UT_wg3_idaghdour.csTop_qtl_results.txt \
+    --features_out_loc /groups/umcg-franke-scrna/tmp04/projects/venema-2022/ongoing/qtl/coeqtl/junk_features.tsv.gz \
+    --co_limix_annotation_prepend /groups/umcg-franke-scrna/tmp04/projects/venema-2022/ongoing/qtl/coeqtl/junk_co
+
 """
 
 #############
