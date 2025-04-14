fc7335f1158965c131000b64218f79e9ba0dd5e4
Roy Oelen <roy.oelen@gmail.com>
2025-04-14 11:05:51 +0200

    - take care of chunks that might have missing columns

--- snakemake-to-map-coeqtls/scripts/coeqtl_merge_outputs.R
+++ snakemake-to-map-coeqtls/scripts/coeqtl_merge_outputs.R
@@ -5,8 +5,8 @@
 # Function: merge nominal coeqtl output files
 # Example usage: 
 # ~/start_Rscript.sh coeqtl_merge_outputs.R \
-#   --input_dir /groups/umcg-franke-scrna/tmp02/projects/venema-2022/ongoing/qtl/coeqtl/output/CD4_T_cells/qtl/ \
-#   --output_file /groups/umcg-franke-scrna/tmp02/projects/venema-2022/ongoing/qtl/coeqtl/output/CD4_T_cells/qtl/CD4_T_cells.tsv.gz
+#   --input_dir /groups/umcg-franke-scrna/tmp04/projects/venema-2022/ongoing/qtl/coeqtl/output/independent/lpmcv2_cell_type_low_filtered/Epithelial/qtl/ \
+#   --output_file /groups/umcg-franke-scrna/tmp04/projects/venema-2022/ongoing/qtl/coeqtl/output/independent/lpmcv2_cell_type_low_filtered/Epithelial/Epithelial.tsv.gz
 #
 ############################################################################################################################
 
@@ -53,6 +53,8 @@ table_per_chunk <- list()
 dirs_input <- list.dirs(input_dir, recursive = F, full.names = F)
 # subset to the ones with the a pattern we expect
 dirs_chunks <- dirs_input[grepl('\\d+_\\d+_\\d+', dirs_input)]
+# we'll save the number of columns per chunk
+ncols_per_chunk <- list()
 # check each chunk
 for (chunk in dirs_chunks) {
   # get the full path to the file
@@ -65,6 +67,38 @@ for (chunk in dirs_chunks) {
     chunk_contents[['chunk']] <- chunk
     # and add to the list
     table_per_chunk[[chunk]] <- chunk_contents
+    # and save the number of columns
+    ncols_per_chunk[[chunk]] <- ncol(chunk_contents)
+  }
+}
+# create a table of the chunks
+ncols_per_chunk_tbl <- data.frame('chunk' = names(ncols_per_chunk), 'ncol' = as.vector(unlist(ncols_per_chunk)))
+# get the unique numbers of chunks
+unique_ncols_per_chunk <- unique(ncols_per_chunk_tbl[['ncol']])
+# if there is more than one, we have to check some stuff
+if (length(unique_ncols_per_chunk) > 1) {
+  # give a warning
+  warning('chunks with smaller number of columns found than the max number of columns, will pad the missing columns onto the smaller chunks\n')
+  # take the biggest number of columns
+  max_col_number <- max(unique_ncols_per_chunk)
+  # take the first region with that number of columns
+  region_cols_max <- ncols_per_chunk_tbl[ncols_per_chunk_tbl[['ncol']] == max_col_number, 'chunk'][1]
+  # get the column names for that one
+  colnames_max <- colnames(table_per_chunk[[region_cols_max]])
+  # now get all the regions that have less than this number
+  regions_not_cols_max <- ncols_per_chunk_tbl[ncols_per_chunk_tbl[['ncol']] < max_col_number, 'chunk']
+  # check each of those regions
+  for (region in regions_not_cols_max) {
+    # get the info for that region
+    region_not_max <- table_per_chunk[[region]]
+    # get the missing columns
+    missing_columns <- setdiff(colnames_max, colnames(region_not_max))
+    # add those missing columns
+    for (missing_column in missing_columns) {
+      region_not_max[[missing_column]] <- NA
+    }
+    # and put back into the list
+    table_per_chunk[[region]] <- region_not_max
   }
 }
 # now combine all the chunks
